f799abea93f5eb10641b5c4ef71850e1032353f7
Anibal Morales <amora197@fiu.edu>
2020-12-01 11:17:51 +0100

    erased "Tutorial" from index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ release = '1.0'
 
 # install the needed theme with:
 # pip install sphinx_ops_theme
-#import sphinx_ops_theme
+import sphinx_ops_theme
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -37,7 +37,6 @@ release = '1.0'
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
-    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,13 +56,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 master_doc = 'index'
 
-#html_theme = "sphinx_ops_theme"
-html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_ops_theme.get_html_theme_path()]
+html_theme = "sphinx_ops_theme"
+#html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_ops_theme.get_html_theme_path()]
 html_add_permalinks = ""
 
 html_theme_options = {
-    'collapse_navigation': False,
     'sticky_navigation': False,
+    'collapse_navigation': False,
 }
 
